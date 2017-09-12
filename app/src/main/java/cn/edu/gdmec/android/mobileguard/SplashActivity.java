e4e7b38307ffea2fcdcecdc8d19a79727b21c8d7
Student <student@Student-34.local>
2017-09-12 09:04:27 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.android.mobileguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+    //我在这里协商注释
+    //嘿嘿嘿嘿
+}
