44f3747a75e1595f2281b97dfce9b492e3032ce5
chenwenqin陈文钦 <31615590+vincen11@users.noreply.github.com>
2017-11-26 20:12:11 +0800

    1201创好文件

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/TrafficMonitoringActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m8trafficmonitor;
+
+/**
+ * Created by Asus on 2017/12/1.
+ */
+
+public class TrafficMonitoringActivity {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/db/TrafficOpenHelper.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m8trafficmonitor.db;
+
+/**
+ * Created by Asus on 2017/12/1.
+ */
+
+public class TrafficOpenHelper {
+}
